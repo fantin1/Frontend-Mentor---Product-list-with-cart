da2b2e7edc3ee0629b0ae6f2321882c8ec541b67
p-fan <p-fantin@hotmail.com>
2025-05-09 17:27:44 -0300

    cart

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 'use strict'
 
+const dessertsGrid = document.querySelector('.desserts-grid');
+
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        const products = json;
+        //populateDesserts(products);
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    })
+
+
+function populateDesserts(products) {
+    dessertsGrid.innerHTML = '';
+    products.forEach((prod) => {
+        const html = `
+        <div class="dessert">
+                    <div>
+                        <img  class="dessert-img" src="${prod.image.desktop}" alt="">
+                    </div>
+                    <div>
+                        <button class="cart-btn">
+                            <img src="assets/images/icon-add-to-cart.svg" alt="">
+                            Add to Cart
+                        </button>
+                    </div>
+                    <div class="dessert__text">
+                        <h2 class="dessert-type">${prod.category}</h2>
+                        <h3 class="dessert-name">${prod.name}</h3>
+                        <p class="price">$${prod.price.toFixed(2)}</p>
+                    </div>
+                </div>`
+        dessertsGrid.insertAdjacentHTML("beforeend", html);
+    })
+}
